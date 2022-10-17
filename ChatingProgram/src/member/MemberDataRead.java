7c0c24c2d642431576e582eb81cb03fc1fd8cd22
강승석 <ksssbl@naver.com>
2022-10-17 16:10:38 +0900

    멤버 수정(교수님 피드백) - MemberDataRead 추가

--- ChatingProgram/src/member/MemberDataRead.java
+++ ChatingProgram/src/member/MemberDataRead.java
@@ -1,17 +1,23 @@
 package member;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MemberDataRead {
+	//메소드: 메인
 	@SuppressWarnings("null")
 	public static void main(String[] args) {	
 		try {
 			String MEMBER_FILE_NAME = "c:\\temp\\member.db";
 			ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(MEMBER_FILE_NAME)));
-			@SuppressWarnings("unchecked")
 			List<Member> memberList = (List<Member>) in.readObject();
 			memberList.stream().forEach(m -> System.out.println(m));
 		
